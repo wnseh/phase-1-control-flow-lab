628f969b4865205477d7108678788ef2b3dbd6de
Young Jun Do <youngjundoe@gmail.com>
2021-09-07 15:45:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  // // Write your code here!
+  // if (distance <= 400){
+  //   return "This one is on me!"
+  // }
+  switch(true){
+    case distance<=400:
+      return "This one is on me!";
+    case distance>2500:
+      return "No can do."
+      break;
+    case distance>2000:
+      return "I will gladly take your thirty bucks.";
+      break;
+
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  switch(true){
+    case city === "NYC":
+      return "Ok, sounds good."
+    default:
+      return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(true){
+    case tip === "generous":
+      return "Thank you so much."
+    case tip === "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+
+  }
 }
